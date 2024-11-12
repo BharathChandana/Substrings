3a2d2a42d018ad905bf8f51f9b3719286f581d9e
HP <HP@DESKTOP-4G77UKH>
2024-11-12 11:13:38 +0530

    Add project files.

---
+++ Substrings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Substrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string :");
+            string input=Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = i; j < input.Length; j++)
+                {
+                    for (int k = i; k <= j; k++)
+                    {
+                        Console.Write(input[k]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
